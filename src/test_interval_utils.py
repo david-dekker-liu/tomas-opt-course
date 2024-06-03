803b96abb17d1655d579d20c82b1174577c4be74
davde78 <david.dekker@liu.se>
2024-06-03 23:11:44 +0200

    Try to get build to work by testing in src folder

--- src/test_interval_utils.py
+++ src/test_interval_utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from src.intervals import Interval, LinkedInterval
-import src.interval_utils as interval_utils
+from intervals import Interval, LinkedInterval
+import interval_utils as interval_utils
 import pytest
 
 
