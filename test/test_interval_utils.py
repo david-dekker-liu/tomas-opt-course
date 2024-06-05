22986ee28ae0882d5f420fb898c0072201018d1b
davde78 <david.dekker@liu.se>
2024-06-05 08:26:33 +0200

    Check single values instead of objects

--- test/test_interval_utils.py
+++ test/test_interval_utils.py
@@ -8,6 +8,10 @@ def d(x):
     return datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
 
 
+def s(x):
+    return x.strftime("%Y-%m-%d %H:%M:%S")
+
+
 def test_update_time_range_basic():
     linked_interval = LinkedInterval(
         start=d("2021-05-11 13:00:00"),
@@ -20,12 +24,12 @@ def test_update_time_range_basic():
 
     updated_linked_interval = interval_utils.update_time_range(linked_interval, new_start, new_end)
 
-    assert updated_linked_interval == LinkedInterval(
-        start=d("2021-05-11 13:10:00"),
-        end=d("2021-05-11 13:20:00"),
-        orig_start=d("2021-05-11 12:10:00"),
-        orig_end=d("2021-05-11 12:20:00")
-    )
+    # I prefer checking separate values over checking whether two objects are the same,
+    # as the error messages are clearer here
+    assert s(updated_linked_interval.start) == "2021-05-11 13:10:00"
+    assert s(updated_linked_interval.end) == "2021-05-11 13:20:00"
+    assert s(updated_linked_interval.orig_start) == "2021-05-11 12:10:00"
+    assert s(updated_linked_interval.orig_end) == "2021-05-11 12:20:00"
 
 
 def test_update_time_range_date_boundary():
@@ -40,12 +44,10 @@ def test_update_time_range_date_boundary():
 
     updated_linked_interval = interval_utils.update_time_range(linked_interval, new_start, new_end)
 
-    assert updated_linked_interval == LinkedInterval(
-        start=d("2021-05-10 20:00:02"),
-        end=d("2021-05-11 03:15:00"),
-        orig_start=d("2021-05-09 04:10:14"),
-        orig_end=d("2021-05-09 11:25:12")
-    )
+    assert s(updated_linked_interval.start) == "2021-05-10 20:00:02"
+    assert s(updated_linked_interval.end) == "2021-05-11 03:15:00"
+    assert s(updated_linked_interval.orig_start) == "2021-05-09 04:10:14"
+    assert s(updated_linked_interval.orig_end) == "2021-05-09 11:25:12"
 
 
 # Test for an exception if the interval is not properly shortened
@@ -94,13 +96,27 @@ def test_intersect_intervals():
 
     interval_1, interval_2, interval_3, interval_4, interval_5 = intersected_spaces
 
-    assert interval_1 == LinkedInterval(
-        start=d("2021-05-08 14:30:00"),
-        end=d("2021-05-08 14:40:00"),
-        orig_start=d("2021-05-08 04:05:00"),
-        orig_end=d("2021-05-08 04:15:00"))
+    assert s(interval_1.start) == "2021-05-08 14:30:00"
+    assert s(interval_1.end) == "2021-05-08 14:40:00"
+    assert s(interval_1.orig_start) == "2021-05-08 04:05:00"
+    assert s(interval_1.orig_end) == "2021-05-08 04:15:00"
 
-    assert interval_2 == LinkedInterval(d("2021-05-08 18:00:00"), d("2021-05-08 18:12:00"), d("2021-05-08 06:04:30"), d("2021-05-08 06:16:30"))
-    assert interval_3 == LinkedInterval(d("2021-05-08 18:20:00"), d("2021-05-08 18:40:00"), d("2021-05-08 06:24:30"), d("2021-05-08 06:44:30"))
-    assert interval_4 == LinkedInterval(d("2021-05-08 18:40:00"), d("2021-05-08 18:45:00"), d("2021-05-08 06:44:30"), d("2021-05-08 06:49:30"))
-    assert interval_5 == LinkedInterval(d("2021-05-08 20:05:10"), d("2021-05-08 20:10:00"), d("2021-05-08 07:00:00"), d("2021-05-08 07:04:50"))
+    assert s(interval_2.start) == "2021-05-08 18:00:00"
+    assert s(interval_2.end) == "2021-05-08 18:12:00"
+    assert s(interval_2.orig_start) == "2021-05-08 06:04:30"
+    assert s(interval_2.orig_end) == "2021-05-08 06:16:30"
+
+    assert s(interval_3.start) == "2021-05-08 18:20:00"
+    assert s(interval_3.end) == "2021-05-08 18:40:00"
+    assert s(interval_3.orig_start) == "2021-05-08 06:24:30"
+    assert s(interval_3.orig_end) == "2021-05-08 06:44:30"
+
+    assert s(interval_4.start) == "2021-05-08 18:40:00"
+    assert s(interval_4.end) == "2021-05-08 18:45:00"
+    assert s(interval_4.orig_start) == "2021-05-08 06:44:30"
+    assert s(interval_4.orig_end) == "2021-05-08 06:49:30"
+
+    assert s(interval_5.start) == "2021-05-08 20:05:10"
+    assert s(interval_5.end) == "2021-05-08 20:10:00"
+    assert s(interval_5.orig_start) == "2021-05-08 07:00:00"
+    assert s(interval_5.orig_end) == "2021-05-08 07:04:50"
